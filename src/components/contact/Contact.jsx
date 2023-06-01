ca2bab837a2533295ecfbcc0c7ad7a28e4cfa03b
dmitryulasau <dmitrushok@gmail.com>
2023-06-01 18:25:10 -0400

    feat: add contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,46 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 export default function Contact() {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMailOutline className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>dmitrushok@gmail.com</h5>
+            <a
+              href="mailto:dmitrushok@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send a Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
